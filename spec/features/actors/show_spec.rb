6cc1484ca2edef3deabc54229e7d32026de3aaf9
Matt Holmes <holmesm8@gmail.com>
2020-02-15 10:13:15 -0700

    Refactor testing for user story 1 and 2 for best practices

--- spec/features/actors/show_spec.rb
+++ spec/features/actors/show_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "studios index page", type: :feature do
+RSpec.describe "actors show page", type: :feature do
   context "as a visitor" do
+    before :each do
+      @studio1 = Studio.create!(name: "Disney")
+      @movie1 = @studio1.movies.create!(name: "Iron Man", creation_year: "2006", genre: "Superhero")
+      @movie2 = @studio1.movies.create!(name: "Spiderman", creation_year: "2017", genre: "Superhero")
+
+      @actor1 = @movie1.actors.create!(name: "Matt Holmes", age: "30")
+      @actor2 = @movie1.actors.create!(name: "Zeke Clark", age: "26")
+    end
+
     it "can see all studios with the names of their movies listed" do
-      studio1 = Studio.create!(name: "Disney")
-      movie1 = studio1.movies.create!(name: "Iron Man", creation_year: "2006", genre: "Superhero")
-      movie2 = studio1.movies.create!(name: "Spiderman", creation_year: "2017", genre: "Superhero")
 
-      actor1 = movie1.actors.create!(name: "Matt Holmes", age: "30")
-      actor2 = movie1.actors.create!(name: "Zeke Clark", age: "25")
+      visit "/actors/#{@actor1.id}"
 
-      visit "/actors/#{actor1.id}"
+      expect(page).to have_content(@actor1.name)
+      expect(page).to have_content(@actor1.age)
+      expect(page).to have_content(@movie1.name)
 
-      expect(page).to have_content(actor1.name)
-      expect(page).to have_content(actor1.age)
-      expect(page).to have_content(movie1.name)
-
-      expect(page).to_not have_content(actor2.name)
-      expect(page).to_not have_content(movie2.name)
+      expect(page).to_not have_content(@actor2.name)
+      expect(page).to_not have_content(@movie2.name)
     end
   end
 end
--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,23 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "studios index page", type: :feature do
   context "as a visitor" do
-    it "can see all studios with the names of their movies listed" do
-      studio1 = Studio.create!(name: "Disney")
-      studio2 = Studio.create!(name: "Sony")
+    before :each do
+      @studio1 = Studio.create!(name: "Disney")
+      @studio2 = Studio.create!(name: "Sony")
 
-      movie1 = studio1.movies.create!(name: "Iron Man", creation_year: "2006", genre: "Superhero")
-      movie2 = studio2.movies.create!(name: "Spiderman", creation_year: "2017", genre: "Superhero")
-      movie3 = studio1.movies.create!(name: "Toy Story", creation_year: "1994", genre: "Animated")
+      @movie1 = @studio1.movies.create!(name: "Iron Man", creation_year: "2006", genre: "Superhero")
+      @movie2 = @studio2.movies.create!(name: "Spiderman", creation_year: "2017", genre: "Superhero")
+      @movie3 = @studio1.movies.create!(name: "Toy Story", creation_year: "1994", genre: "Animated")
+    end
+
+    it "can see all studios with the names of their movies listed" do
 
       visit '/studios'
 
-      within ("#studio-#{studio1.name}") do
-        expect(page).to have_content("#{movie1.name}")
-        expect(page).to have_content("#{movie3.name}")
+      within ("#studio-#{@studio1.id}") do
+        expect(page).to have_content("#{@studio1.name}")
+        expect(page).to have_content("#{@movie1.name}")
+        expect(page).to have_content("#{@movie3.name}")
       end
 
-      within ("#studio-#{studio1.name}") do
-        expect(page).to have_content("#{movie2.name}")
+      within ("#studio-#{@studio2.id}") do
+        expect(page).to have_content("#{@studio2.name}")
+        expect(page).to have_content("#{@movie2.name}")
       end
     end
   end
